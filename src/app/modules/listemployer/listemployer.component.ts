d0a934f5237d2aae46b1a4478b994d5c1d8d6b53
lachguer <yousseflachguer25@gmail.com>
2022-08-22 02:00:49 +0200

    safi 9adit lblan dyal table ou zedt panel edit

--- src/app/modules/listemployer/listemployer.component.ts
+++ src/app/modules/listemployer/listemployer.component.ts
@@ -1,56 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { ApiiService } from 'src/app/service/apii.service';
-import { PaneleditComponent } from './paneledit/paneledit.component';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  naissance: string;
-  nat: string;
-  poste: string;
-  type: string;
-  dated: string;
-  datef: string;
-  soldec:number;
-  
-  
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'hassan lachguer', naissance: '10/02/2001', nat: 'H', poste:'developpeur',type:'CDI',dated:'10/10/23',datef:'10/10/23',soldec:16,},
-
-];
-
 @Component({
   selector: 'app-listemployer',
   templateUrl: './listemployer.component.html',
   styleUrls: ['./listemployer.component.scss']
 })
-
-
 export class ListemployerComponent implements OnInit {
-  dataArray : any 
-  constructor(public dialog: MatDialog , public ser:ApiiService) {
-    this.ser.afficher().subscribe(data=>this.dataArray=data) 
 
-  }
-
-  openDialog() {
-    this.dialog.open(PaneleditComponent, {
-      width:'30%'
-    });
-  }
-
-  displayedColumns: string[] = ['position', 'name', 'naissance', 'nat','poste', 'type', 'dated','datef','soldec','action'];
-  dataSource = ELEMENT_DATA;
-
+  constructor() { }
 
   ngOnInit(): void {
   }
-  
 
 }
-
-
-
