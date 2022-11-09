79c2f2b92222b7e59ad053eef6b825be21008b1c
AIT EL BACHA <r.aitelbacha@gmail.com>
2022-11-09 01:16:52 +0100

    service api >>

--- src/app/service/apii.service.ts
+++ src/app/service/apii.service.ts
@@ -11,27 +11,33 @@ export class ApiiService {
   constructor(private http:HttpClient ) {}
 
 
-
-  afficher()
-  {
-
-return this.http.get('http://localhost:4000/result_emploiyeur');
-
-
-}
-
-adddemande(add_dem:any) : Observable<any> {
- 
-  return this.http.post('http://localhost:4000/ajouterdemande',add_dem)
-
- }
- addduser(add_emp:any) : Observable<any> {
-  return this.http.post('http://localhost:4000/ajouter_emploiyeur',add_emp)
-
- }
+//>>>>>>>>>>>>>>>>>>>>>>>>>>  conge 
+//nombre de conge 
+nombreConge() :Observable<any>{ return this.http.get('http://localhost:8081/nombreconge')};
+//sans decision 
+Congesansdecision() :Observable<any>{ return this.http.get('http://localhost:8081/getcongesansdecision')};
+//conge accepter  
+CongeAccepte() :Observable<any>{ return this.http.get('http://localhost:8081/getCongeAcceptrer')};
+//conge refuser  
+CongeRefuser() :Observable<any>{ return this.http.get('http://localhost:8081/getCongeRefuser')};
+//historiqueconge
+Congehistorique() :Observable<any>{ return this.http.get('http://localhost:8081/getCongeRefuser')};
+//Post conge refuser 
+postCongeAccepter(id_conge:any) :Observable<any>{ return this.http.get('http://localhost:8081/postcongeaccepter',id_conge)};
+// post conge accepter 
+postCongeRefuser(id_conge:any) :Observable<any>{ return this.http.get('http://localhost:8081/postcongerefuser',id_conge)};
 
 
-alldemande() :Observable<any>{ return this.http.get('http://localhost:8081/getAllconge')};
+//>>>>>>>>>>>>>>>>>>>><login 
+//token 
+logine (user:any): Observable<any> { return this.http.post('http://localhost:8081/login',user)}
+//garde Admin
+accederadmin(){return !!localStorage.getItem('token');}
+//garde User
+ accederuser(){ return !!localStorage.getItem('token');}
+
+
+
 
  afficheruser()
  {
@@ -66,31 +72,5 @@ alldemande() :Observable<any>{ return this.http.get('http://localhost:8081/getAl
   return this.http.delete('http://localhost:4000/delet_emploiyeur/'+_id)
 
  }
-
-
-logine (user:any): Observable<any> { 
-
-  return this.http.post('http://localhost:8081/login',user)
-
-}
-
-
-
-accederadmin()
- {
-  return !!localStorage.getItem('token');
- }
-
-
- accederuser()
- {
-  return !!localStorage.getItem('token');
-  
- }
-
-
-
- 
-
 
 }
